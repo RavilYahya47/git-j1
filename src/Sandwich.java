8dcece0e3bdc8c667998a975c8b6b48200962c86
Teymur Fazilov <fazilovteymur4@gmail.com>
2026-02-16 20:48:51 +0400

    TimCom1

---
+++ src/Sandwich.java
@@ -0,0 +1,32 @@
+public class Sandwich {
+    private String Bread;
+    private String Meat;
+    private String Cheese;
+    public void addBread (){
+
+    }
+
+    public String getBread() {
+        return Bread;
+    }
+
+    public void setBread(String bread) {
+        Bread = bread;
+    }
+
+    public Private getString() {
+        return String;
+    }
+
+    public void setString(Private string) {
+        String = string;
+    }
+
+    public String getMeat() {
+        return Meat;
+    }
+
+    public void setMeat(String meat) {
+        Meat = meat;
+    }
+}
